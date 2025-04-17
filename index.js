71af6ad6ab9b7c591b68a6fb446fa56184b3fb2f
unknown <lokchanmukhiya@gmail.com>
2025-04-17 11:51:56 +0545

    adding search feature

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 var inps = document.querySelector("#search");
+var searchinp = document.querySelector("#searchInp");
 var btn = document.querySelector("button")
 var ul = document.querySelector("#uli")
 var li;
+
+
+
+var task = [];
+
+function adding(){
+
+        li = document.createElement("li");
+        li.textContent = inps.value;
+        ul.appendChild(li);
+        inps.value = '';
+   
+}
+
 
 btn.addEventListener("click", function(){
     if(inps.value.trim() === ''){
         alert("add your task please");
     }
     else{
-        li = document.createElement("li");
-        li.textContent = inps.value;
-        ul.appendChild(li);
-        inps.value = '';
+        task.push({ name: inps.value });
+        adding()
     }
+
+    inps.value = '';
+    searchinp.value = '';
 })
+
+searchinp.addEventListener("input", function () {
+    var tasks = task.filter(function (e) {
+        return e.name.toLowerCase().includes(searchinp.value.toLowerCase());
+    });
+
+    ul.innerHTML = ""; // clear the list
+    tasks.forEach(function (elem) {
+        li = document.createElement("li");
+        li.textContent = elem.name;
+        ul.appendChild(li);
+
+    })
+    });
